ade010a76dfa4adb280e84e2a7e082a5304b707f
KarmitP98 <karmit199@gmail.com>
2020-11-13 23:37:56 -0500

    Updated Play List

--- Play.py
+++ Play.py
@@ -10,17 +10,17 @@ def run(video_url, title):
     V_EXT = ".mp4"
     A_EXT = ".mp3"
 
-    print(video_url)
-
     try:
         yt = pt.YouTube(video_url)
         # Set File-Name
         file_name = re.sub("\W+", " ", yt.title).title()
+        print("Downloading...")
+        print(file_name + V_EXT)
         # Get the first stream (Usually the lowest quality, we just care about the audio here so it is fine!)
         video = yt.streams.first()
         # Download it at this location
         video.download(PATH, file_name)
-
+        print("Converting Video to Audio...")
         # Convert the video into an audio
         vid = VideoFileClip(os.path.join(PATH + file_name + V_EXT))
         vid.audio.write_audiofile(os.path.join(PATH + file_name + A_EXT))
@@ -32,20 +32,17 @@ def run(video_url, title):
         print(e)
 
 
-def download(playlist_url):
-    playlist_url = playlist_url.split("::")
-    playlist = Playlist(playlist_url[0])
+def download(playlist_url, playListName):
+    playlist = Playlist(playlist_url)
     # this fixes the empty playlist.videos list
     playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-    playlist.download_all()
-
     for url in playlist.video_urls:
-        run(url, playlist_url[1])
+        run(url, playListName)
 
 
 def start():
-    download(input("Enter the Youtube Playlist URL: "))
+    download(input("Enter the Youtube Playlist URL: "), input("Enter the playlist Name: "))
 
 
 if __name__ == "__main__":
