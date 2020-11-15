e799dc51f891c46bfb7fbc2c65adf7ef62f1d437
Karmit Personal <karmit199@gmail.com>
2020-11-14 23:41:26 -0500

    Minor Changes

--- playlist_download.py
+++ playlist_download.py
@@ -5,10 +5,9 @@ from moviepy.editor import *
 from pytube import Playlist
 
 
-def run(video_url, title):
-    PATH = "C:/Users/karmi/Desktop/Youtube/" + title + "/"
+def download_video(video_url: str, play_list_name: str):
+    PATH = "C:/Users/karmi/Desktop/Youtube/" + play_list_name + "/"
     V_EXT = ".mp4"
-    A_EXT = ".mp3"
 
     try:
         yt = pt.YouTube(video_url)
@@ -20,29 +19,43 @@ def run(video_url, title):
         video = yt.streams.first()
         # Download it at this location
         video.download(PATH, file_name)
-        print("Converting Video to Audio...")
-        # Convert the video into an audio
-        vid = VideoFileClip(os.path.join(PATH + file_name + V_EXT))
-        vid.audio.write_audiofile(os.path.join(PATH + file_name + A_EXT))
-        vid.close()
-
-        os.remove(PATH + file_name + V_EXT)
+        return file_name
 
     except Exception as e:
         print(e)
 
 
-def download(playlist_url, playListName):
+def convert_video(file_name, play_list_name: str):
+    PATH = "C:/Users/karmi/Desktop/Youtube/" + play_list_name + "/"
+    V_EXT = ".mp4"
+    A_EXT = ".mp3"
+
+    print("Converting " + file_name + " to Audio...")
+    # Convert the video into an audio
+    vid = VideoFileClip(os.path.join(PATH + file_name + V_EXT))
+    vid.audio.write_audiofile(os.path.join(PATH + file_name + A_EXT))
+    vid.close()
+    os.remove(PATH + file_name + V_EXT)
+
+
+def download(playlist_url: str, play_list_name: str):
     playlist = Playlist(playlist_url)
     # this fixes the empty playlist.videos list
     playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
+    files = []
+
     for url in playlist.video_urls:
-        run(url, playListName)
+        files.append(download_video(url, play_list_name))
+
+    print(files)
+
+    for file in files:
+        convert_video(file_name=file, play_list_name=play_list_name)
 
 
 def start():
-    download(input("Enter the Youtube Playlist URL: "), input("Enter the playlist Name: "))
+    download(playlist_url=input("Enter the Youtube Playlist URL: "), play_list_name=input("Enter the playlist Name: "))
 
 
 if __name__ == "__main__":
