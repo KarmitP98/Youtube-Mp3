e8405a0108cb670efdd55ea8a21253fe88733d19
Karmit Personal <karmit199@gmail.com>
2021-03-09 20:29:48 -0500

    Update on Implementation.

--- video_download.py
+++ video_download.py
@@ -1,36 +1,76 @@
+import os
 import re
 
 import pytube as pt
 from moviepy.editor import *
 
 
-def run():
-    PATH = "C:/Users/karmi/Desktop/Youtube/"
-    V_EXT = ".mp4"
-    A_EXT = ".mp3"
+def progress_Check(chunk=None, file_handle=None, remaining=None):
+    # Gets the percentage of the file that has been downloaded.
+    percent = (100 * (file_size - remaining)) / file_size
+    if percent % 1 == 0:
+        print("{:00.0f}% downloaded".format(percent))
 
-    video_url = input("Enter Youtube Vide URL: ")
 
+def getFilePath():
+    home = os.path.expanduser('~')
+    download_path = os.path.join(home, 'Desktop\\Youtube\\')
+    return download_path
+
+
+def videoToAudio():
+    global file_name
+    print("Converting mp4 to mp3...")
+    # Convert the video into an audio
+    path = getFilePath()
+    vid = VideoFileClip(os.path.join(path + file_name + V_EXT))
+    vid.audio.write_audiofile(os.path.join(path + file_name + A_EXT))
+    vid.close()
+    print("Audio Successfully Converted!")
+    os.remove(path + file_name + V_EXT)
+
+
+def getVideoFromYoutube(video_url):
+    global file_name, file_size
     try:
         yt = pt.YouTube(video_url)
+
+        print("Video Title: " + yt.title)
+        print("Video Author: " + yt.author)
+
+        print("\nDownloading video...")
+
         # Set File-Name
-        file_name = re.sub("\W+"," ", yt.title).title()
-        # Get the first stream (Usually the lowest quality, we just care about the audio here so it is fine!)
-        video = yt.streams.first()
-        # Download it at this location
-        video.download(PATH, file_name)
+        file_name = re.sub("\W+", " ", yt.title).title()
 
-        # Convert the video into an audio
-        vid = VideoFileClip(os.path.join(PATH + file_name + V_EXT))
-        vid.audio.write_audiofile(os.path.join(PATH + file_name + A_EXT))
-        vid.close()
+        video = pt.YouTube(video_url, on_progress_callback=progress_Check)
 
-        os.remove(PATH + file_name + V_EXT)
+        video_type = video.streams.filter(progressive=True, file_extension="mp4").first()
+
+        file_size = video_type.filesize
+
+        video_type.download(getFilePath(), file_name)
 
     except Exception as e:
-        print(e)
+        print("Video not found!")
+
+    videoToAudio()
+
+
+def run():
+    # PATH = "C:/Users/karmi/Desktop/Youtube/"
+
+    video_url = input("Enter Youtube Vide URL: ")
+    print(video_url)
+
+    getVideoFromYoutube(video_url)
 
 
 if __name__ == "__main__":
     while 1:
         run()
+
+V_EXT = ".mp4"
+A_EXT = ".mp3"
+file_size = 0
+file_name = ""
