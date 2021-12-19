4a2f016f7115cebc69dfeb00511a3457f03e07f1
KarmitP98 <karmit199@gmail.com>
2021-12-19 13:38:12 -0500

    Updated single video download function

--- updateVideoDownload.py
+++ updateVideoDownload.py
@@ -1,23 +1,42 @@
 import os
+import re
 
 from pytube import YouTube
 
+V_EXT = '.mp4'
+A_EXT = '.mp3'
+file_name = ''
+file_size = 0
+progress = 0
+DOWNLOAD_LOCATION = 'Downloads\\Youtube\\'
+
 
 # on_progress_callback takes 4 parameters.
-def progress_Check(stream=None, chunk=None, file_handle=None, remaining=None):
+def progress_check(chunk, file_handle, bytes_remaining=None):
     # Gets the percentage of the file that has been downloaded.
-    percent = (100 * (file_size - remaining)) / file_size
-    print("{:00.0f}% downloaded".format(percent))
+    global file_size, progress
+    progress = (100 * (file_size - bytes_remaining)) / file_size
+    print("{:00.0f}% downloaded".format(progress))
+
+
+def sanitize_filename(name):
+    global file_name
+    file_name = re.sub("[^A-Za-z0-9]+", " ", name)
+    file_name = re.sub('^\s+', "", file_name)
+    file_name = re.sub("$\s+", "", file_name)
+    return file_name
 
 
 # Grabs the file path for Download
 def file_path():
+    global DOWNLOAD_LOCATION
     home = os.path.expanduser('~')
-    download_path = os.path.join(home, 'Desktop\\Youtube\\')
+    download_path = os.path.join(home, DOWNLOAD_LOCATION)
     return download_path
 
 
 def start():
+    global V_EXT, file_size
     print("Your video will be saved to: {}".format(file_path()))
     # Input
     yt_url = input("Copy and paste your YouTube URL here: ")
@@ -26,27 +45,28 @@ def start():
 
     # Searches for the video and sets up the callback to run the progress indicator.
     try:
-        video = YouTube(yt_url, on_progress_callback=progress_Check)
+        video = YouTube(yt_url, on_progress_callback=progress_check)
     except:
         print("ERROR. Check your:\n  -connection\n  -url is a YouTube url\n\nTry again.")
         start()
 
     # Get the first video type - usually the best quality.
-    video_type = video.streams.filter(progressive=True, file_extension="mp4").first()
+    video_type = video.streams.filter(type='video').get_highest_resolution()
 
     # Gets the title of the video
     title = video.title
 
     # Prepares the file for download
-    print("Fetching: {}...".format(title))
-    global file_size
+    print("Fetching: {} ...".format(title))
     file_size = video_type.filesize
     # Starts the download process
-    video_type.download(file_path(), input("Do you also want to download the video?"))
+    try:
+        video_type.download(output_path=file_path(), filename=sanitize_filename(title) + V_EXT, skip_existing=True,
+                            max_retries=2, timeout=5000)
+        print("Video successfully downloaded at {}".format(file_path() + file_name + V_EXT))
+    except:
+        print("Error downloading the video!\nPlease try again!")
 
-    print("Ready to download another video.\n\n")
+
+if __name__ == "__main__":
     start()
-
-
-file_size = 0
-start()
